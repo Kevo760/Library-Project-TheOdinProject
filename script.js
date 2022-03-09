1bbad9430c0bfe7758559e94738ad2ad0f0069a3
TheLegend760 <73809530+TheLegend760@users.noreply.github.com>
2022-03-09 01:01:22 -0700

    final v1.0.0
    HTML
    -Deleted mock up books
    
    CSS
    -Changed background color
    -Added mobile styling
    
    Javascript
    -Added read handler
    -Buttons added by dom
    -Created remove button function to be able to remove data from array and box design

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 
 let myLibary = [];
 
-// book class
+// Book class
 function Book(title, artist, pages) {
 this.title = title;
 this.artist = artist;
 this.pages = pages;
+this.read = false;
 };
+
+// Puts read to true
+Book.prototype.haveRead = function() {
+    this.read = true;
+};
+
+// put read to false
+Book.prototype.notRead = function() {
+    this.read = false;
+};
+
 
 
 
@@ -38,9 +50,20 @@ document.querySelector('#formBook').addEventListener('submit', (e)=> {
     const title = document.querySelector('#title').value;
     const artist = document.querySelector('#artist').value;
     const pages = document.querySelector('#pages').value;
+  
 
     // Add them to library
     const book = new Book(title, artist, pages);
+
+    // Handles read or not read
+    const readCheckbox = document.querySelector('#haveRead');
+    if(readCheckbox.checked == true) {
+        book.haveRead();
+    } else {
+        book.notRead();
+    };
+
+    // Pushes book to library
     myLibary.push(book);
     addBookStyle(book);
     modalBg.style.display = 'none';  
@@ -52,6 +75,7 @@ function clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#artist').value = '';
     document.querySelector('#pages').value = '';
+    document.querySelector('#haveRead').checked = false;
 }
 
 
@@ -59,34 +83,99 @@ function clearFields() {
 function addBookStyle(a) {
     const bookBox = document.querySelector('.book-box');
     const addBookDiv = document.createElement('div');
-    addBookDiv.className = 'book';
+    addBookDiv.className = `book ${a.title}`;
 
     addBookDiv.innerHTML = `
-    <p class="top-text">Title: <span class="title">${a.title}</span></p>
+    <p class="top-text ${a.title}">Title: <span class="title">${a.title}</span></p>
     <p>Artist: <span class="artist">${a.artist}</span></p>
     <p>Pages: <span class="pages">${a.pages}</span></p>
-    <div class="btn">
-        <button class="read-status">Read</button>
-        <button class="remove" onclick="deleteBook('${a.title}')">Remove</button>
-    </div>
     `;
+
+    // Creates btn div section for book
+    const btnDiv = document.createElement('div');
+    btnDiv.classList.add('btn');
+    addBookDiv.appendChild(btnDiv);
+
+    // Creates read button
+    if(a.read === true) {
+        // if book.read is true create button that will put it to to true
+        const readStatusBtn = document.createElement('button');
+        readStatusBtn.classList.add('read-status');
+        readStatusBtn.innerText = 'Read';
+        readStatusBtn.style.backgroundColor = 'hsl(147, 50%, 47%)';
+        // Click Event that changes button to read or not read and adjust the array
+        readStatusBtn.addEventListener('click', function() {
+            if(a.read === true) {
+                readStatusBtn.innerText = 'Not Read';
+                readStatusBtn.style.backgroundColor = 'hsl(0, 0%, 85%)'; 
+                a.notRead();
+                console.table(myLibary);
+            } else {
+                readStatusBtn.innerText = 'Read';
+                readStatusBtn.style.backgroundColor = 'hsl(147, 50%, 47%)';
+                a.haveRead();
+                console.table(myLibary);
+            }
+        });
+        btnDiv.appendChild(readStatusBtn);
+    } else {
+        const readStatusBtn = document.createElement('button');
+        readStatusBtn.classList.add('read-status');
+        readStatusBtn.innerText = 'Not Read';
+        readStatusBtn.style.backgroundColor = 'hsl(0, 0%, 85%)';
+         // Click Event that changes button to read or not read and adjust the array
+        readStatusBtn.addEventListener('click', function() {
+            if(a.read === true) {
+                readStatusBtn.innerText = 'Not Read';
+                readStatusBtn.style.backgroundColor = 'hsl(0, 0%, 85%)'; 
+                a.notRead();
+            } else {
+                readStatusBtn.innerText = 'Read';
+                readStatusBtn.style.backgroundColor = 'hsl(147, 50%, 47%)';
+                a.haveRead();
+            }
+        });
+        btnDiv.appendChild(readStatusBtn);
+    }
+
+
+
+    // Creates remove button
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove');
+    removeBtn.innerHTML = "Remove";
+    // Function that attaches the deleteBook on click
+    btnDiv.appendChild(removeBtn);
+    removeBtn.addEventListener('click', function(){
+        deleteBook(a);
+    });
+
     bookBox.appendChild(addBookDiv);
 };
 
-// deletes books from myLibrary
+// deletes book array from myLibrary
 function deleteBook() {
-    let book = myLibary.findIndex(e => e.title == title);
+    let book = myLibary.findIndex(a => a.title == title);
     myLibary.splice(book,1);
 };
 
 
+// Deletes book design
+const bookBox = document.querySelector('.book-box');
+bookBox.addEventListener('click', deleteBox);
+function deleteBox(e) {
+    const item = e.target;
+    if(item.classList[0] === 'remove') {
+        item.parentElement.parentElement.remove();
+    }
+};
 
-// read button
 
-function checkIfRead() {
-    const readStatus = document.querySelector('.read-status');
-    readStatus.addEventListener('click', function(){
-        if(read == 'false')
-    })
-}
+
+
+
+
+
+
+
 
